e6d90e96244f210157726521d0a50cfbc484a7a7
Saurab Rakshit <henryselva@protonmail.com>
2022-01-11 21:49:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <>
+           <h1>App page</h1> 
+           <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati numquam nisi inventore rerum ullam, id nobis cupiditate doloribus totam voluptates! Nisi corporis reiciendis rem obcaecati minima, beatae velit ut maiores repudiandae. Aliquid, asperiores reiciendis rerum sint expedita praesentium quos dignissimos impedit vel? Eius inventore dicta commodi aliquam harum rem sint ad voluptatibus nulla optio, officia id ipsa itaque a neque saepe molestiae nobis illo assumenda, facere magnam! Cumque repellat, sequi nulla voluptatem ad aperiam sed fugit dolor maxime nam corrupti laboriosam iusto id dolore tempore obcaecati eius unde, eum aliquid alias exercitationem! Omnis, aspernatur ipsa enim modi laudantium repellendus ullam odit eos totam tenetur nobis, repellat ducimus quis provident. Nemo voluptates vero, illo laboriosam assumenda, nobis accusantium eius adipisci repudiandae ratione aliquam officiis reprehenderit consequatur enim dicta ducimus quidem itaque. Officiis omnis optio amet odit. Natus ut laudantium voluptatibus laboriosam maiores, velit minus voluptatum, aliquid quis, aut nihil? Quae dolorum est corrupti magni, unde facere libero facilis eligendi quod dicta necessitatibus quam delectus molestiae dolore, veritatis expedita ut eos aliquid nisi error, nobis ad quaerat pariatur ea! Quisquam animi mollitia obcaecati incidunt optio nemo error nobis vel nam, possimus temporibus ipsa suscipit dolore odit adipisci? Et quos illo accusamus aliquam?</h3>
+        </>
+    )
+}
+
+export default App
